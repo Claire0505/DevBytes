c1de98073cff4784e53915c76ebdff1a11086b78
Claire <a0505kimo@hotmail>
2021-06-23 19:07:21 +0800

    加入Android Dependencies implementation Library https://docs.google.com/document/d/1wrf1m4p5wgrRGuasOgPMjEi_IREr8Ni8y1CA-ssnfgQ/edit#heading=h.giyuwfakhyys

---
+++ app/src/main/java/com/example/devbytes/ui/DevByteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.devbytes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.devbytes.R
+
+class DevByteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dev_byte_viewer)
+    }
+}
