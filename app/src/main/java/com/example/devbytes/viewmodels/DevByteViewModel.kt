89e0eb6a8d6b43327bb30a2d30ba78ed183e54cb
Claire <a0505kimo@hotmail>
2021-07-09 20:46:34 +0800

    使用刷新策略集成存儲庫 [https://developer.android.com/codelabs/kotlin-android-training-repository#7]
    在此任務中，您將ViewModel使用簡單的刷新策略將存儲庫與 集成。您從Room數據庫中顯示視頻播放列表，而不是直接從網絡中獲取。
    
    在viewmodels/DevByteViewModel.kt, 內部DevByteViewModel類中，創建一個private名為videosRepository的成員變量VideosRepository。通過傳入單例VideosDatabase對象來實例化變量。
    /**
    * The data source this ViewModel will fetch results from.
    */
    private val videosRepository = VideosRepository(getDatabase(application))
    在DevByteViewModel類中，將refreshDataFromNetwork()方法替換為refreshDataFromRepository()方法。
    舊方法refreshDataFromNetwork()使用 Retrofit 庫從網絡中獲取視頻播放列表。新方法從存儲庫加載視頻播放列表。
    
       /**
         * Refresh data from the repository.
           Use a coroutine launch to run in a
         * background thread.
         */
        private fun refreshDataFromRepository() {
            viewModelScope.launch {
                try {
                    videosRepository.refreshVideos()
                    _eventNetworkError.value = false
                    _isNetworkErrorShown.value = false
    
                }catch (netWorkError: IOException){
                    // Show a Toast error message and hide teh progress bar
                    if (playlist.value.isNullOrEmpty())
                        _eventNetworkError.value = true
                }
            }
        }
    在DevByteViewModel類中的init塊內，將函數調用從 更改refreshDataFromNetwork()為refreshDataFromRepository()。此代碼從存儲庫中獲取視頻播放列表，而不是直接從網絡中獲取。
    init {
       refreshDataFromRepository()
    }
    
    在DevByteViewModel類中，刪除_playlist屬性及其支持屬性playlist。
    private val _playlist = MutableLiveData<List<Video>>()
    ...
    val playlist: LiveData<List<Video>>
       get() = _playlist
    
    在DevByteViewModel類中，實例化對videosRepository像後，添加一個新val調用，playlist用於保存LiveData存儲庫中的視頻列表。
    /**
    * A playlist of videos displayed on the screen.
    */
    val playlist = videosRepository.videos
    運行您的應用程序。該應用程序像以前一樣運行，但現在從網絡獲取 DevBytes 播放列表並保存在Room數據庫中。播放列表從Room數據庫顯示在屏幕上，而不是直接來自網絡。
    如果運行時發生錯誤 Android room persistent: AppDatabase_Impl does not exist 則開啟 build.gradle (app) 加上：
    
        def room_version = "2.3.0"
    
        implementation("androidx.room:room-runtime:$room_version")
        annotationProcessor
        "androidx.room:room-compiler:$room_version"
    
        // To use Kotlin annotation processing tool (kapt)
        kapt("androidx.room:room-compiler:$room_version")

--- app/src/main/java/com/example/devbytes/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/example/devbytes/viewmodels/DevByteViewModel.kt
@@ -7,9 +7,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.example.devbytes.database.getDatabase
 import com.example.devbytes.domain.DevByteVideo
 import com.example.devbytes.network.DevByteNetwork
 import com.example.devbytes.network.asDomainModel
+import com.example.devbytes.respository.VideosRepository
 import kotlinx.coroutines.*
 import java.io.IOException
 
@@ -27,17 +29,21 @@ import java.io.IOException
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
     /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
+     * The data source this ViewModel will fetch results from.
      */
-    private val _playlist = MutableLiveData<List<DevByteVideo>>()
+    private val videosRepository = VideosRepository(getDatabase(application))
 
     /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
+     * A playlist of videos displayed on the screen.
      */
+    val playlist = videosRepository.videos
+
+
+  /*  private val _playlist = MutableLiveData<List<DevByteVideo>>()
+
     val playlist: LiveData<List<DevByteVideo>>
         get() = _playlist
+  */
 
     /**
      * Event triggered for network error. This is private to avoid exposing a
@@ -69,14 +75,36 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
      * init{} is called immediately when this ViewModel is created.
      */
     init {
-        refreshDataFromNetwork()
+        // refreshDataFromNetwork()
+        // 舊方法refreshDataFromNetwork()使用 Retrofit 庫從網絡中獲取視頻播放列表。
+        // 新方法從存儲庫加載視頻播放列表。
+        refreshDataFromRepository()
+    }
+
+    /**
+     * Refresh data from the repository. Use a coroutine launch to run in a
+     * background thread.
+     */
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                videosRepository.refreshVideos()
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            }catch (netWorkError: IOException){
+                // Show a Toast error message and hide teh progress bar
+                if (playlist.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
+        }
     }
 
     /**
      * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
      * background thread.
      */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
+   /* private fun refreshDataFromNetwork() = viewModelScope.launch {
 
         try {
             val playlist = DevByteNetwork.devbytes.getPlaylist()
@@ -89,7 +117,9 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
             // Show a Toast error message and hide the progress bar.
             _eventNetworkError.value = true
         }
-    }
+    }*/
+
+
 
     /**
      * Resets the network error flag.
